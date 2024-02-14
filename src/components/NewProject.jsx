49e1a41d6e48b96bb3389e1b9ad732a24e0fb8c3
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-02-14 22:00:13 +0200

    feat: Add new project component with its input

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,18 +2,22 @@ import Input from "./Input";
 
 function NewProject() {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-3">
         <li>
-          <button>Cancel</button>
+          <button className="text-stone-800 hover:text-[#36506C]">
+            Cancel
+          </button>
         </li>
         <li>
-          <button>Save</button>
+          <button className="px-6 py-2 bg-[#36506C] rounded-lg text-[#EBF7FD] hover:bg-[#233142]">
+            Save
+          </button>
         </li>
       </menu>
       <div>
         <Input label="Title" />
-        <Input label="Description" textarea />
+        <Input label="Description" textArea />
         <Input label="Due Date" />
       </div>
     </div>
